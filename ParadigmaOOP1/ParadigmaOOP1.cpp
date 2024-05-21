868c4abaa4f83dccfbd1b3db6db01da30b0ff73d
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-05-21 10:36:58 +0700

    menampilkan id

--- ParadigmaOOP1/ParadigmaOOP1.cpp
+++ ParadigmaOOP1/ParadigmaOOP1.cpp
@@ -7,6 +7,16 @@ private:
 	const int id;
 	string nama;
 	float nilai; 
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+
+	}
+
+	~mahasiswa() {
+		cout << "Id   = " << id << endl;
+		
+	}
 };
 
 int main() {
